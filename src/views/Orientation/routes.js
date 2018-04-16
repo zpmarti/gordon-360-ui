674e75905e50622ed73930e7ccc8a3c0f8116ad7
annv12321 <anna.pelletier@gordon.edu>
2018-04-15 23:18:19 -0400

    Create routes.js

---
+++ src/views/Orientation/routes.js
@@ -0,0 +1,6 @@
+import About from './views/About';
+import OrientationAll from './views/Orientation';
+import Home from './views/Home';
+import Help from './views/Help';
+import Profile from './views/Profile';
+
